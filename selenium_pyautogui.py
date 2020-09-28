09fda6d9cf0a9ce04ca0368edfa21eecc9b43c49
Lumos Lovegood <50430853+LumosLovegood@users.noreply.github.com>
2020-09-28 10:49:35 +0800

    Just for a test

---
+++ selenium_pyautogui.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @Time : 2020/9/28 10:02
+# @Author: LUMOS
+# @File : selenium_Test.py
+import pyautogui
+from selenium import webdriver
+
+driver = webdriver.Chrome('F:/#Resource/Tools/chromedriver.exe')
